6407e5bbb624c3025c066645756af916e44af624
tak <wontaek22@gmail.com>
2023-03-12 18:13:04 +0900

    23.03.12
    백준 25314번 문제 (반복문 : 코딩은 체육과목 입니다)

---
+++ src/com/tak/study/Main.java
@@ -0,0 +1,21 @@
+package com.tak.study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int answer = n/4;
+
+        System.out.println("long ".repeat(answer) + "int");
+
+    }
+
+}
